bfed99b95d899fa3d65c778071dc86802faa8f4b
ChoiJinYong1 <ifboy@naver.com>
2018-11-29 18:35:13 +0900

    20181129 v2

--- nomuData/src/test/com/test/test.java
+++ nomuData/src/test/com/test/test.java
@@ -1,51 +1,101 @@
 package test.com.test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
 
-
-
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 
 public class test {
-	public static void main(String[] args) {
-        System.out.println("----------------------------------------------------------");
-        System.out.println("JSON String »ý¼º");
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("name", "À¯Çõ");
-        //    "name":"À¯Çõ"
-        JSONArray jsonArray = new JSONArray();
-        JSONObject school = new JSONObject();
-        school.put("subject1", "math");
-        school.put("subject2", "korean");
-        jsonArray.add(school);
-        //    "school":[{"subject1":"math","subject2":"korean"}]
-         
-        jsonObject.put("school", jsonArray);
-         
-        System.out.println(jsonObject.toString());
-         
-        System.out.println("----------------------------------------------------------");
-        System.out.println("JSON String ÆÄ½Ì");
-        try{
-            JSONArray returnSchool = (JSONArray)jsonObject.get("school");
-            for(int i=0;i<returnSchool.size();i++){
-                JSONObject returnSubject = (JSONObject) returnSchool.get(i);
-                System.out.println("subject : "+returnSubject.get("subject1"));
-                System.out.println("subject : "+returnSubject.get("subject2"));
-            }
-            String name = (String)jsonObject.get("name");
-            System.out.println("name : " + name );
-             
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        System.out.println("----------------------------------------------------------");
-    }
+
+	private static final String USER_AGENT = "Mozilla/5.0";
+
+	private static final String GET_URL = "http://hmkcode.appspot.com/jsonservlet";
+
+	private static final String POST_URL = "http://nomu.hyunjang.co.kr:8001/NomuApiService/rest/web/login";
+	private static final String POST_URL2 = "http://nomu.hyunjang.co.kr:8001/NomuApiService/rest/web/workercommon";
+	//private static final String POST_URL = "http://httpbin.org/post";
+
+	public static void main(String[] args) throws IOException {
+		sendGET();
+		System.out.println("GET DONE");
+		sendPOST();
+		System.out.println("POST DONE");
+	}
+
+	private static void sendGET() throws IOException {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet(GET_URL);
+		httpGet.addHeader("User-Agent", USER_AGENT);
+		CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+
+		System.out.println("GET Response Status:: "
+				+ httpResponse.getStatusLine().getStatusCode());
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				httpResponse.getEntity().getContent()));
+
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = reader.readLine()) != null) {
+			response.append(inputLine);
+		}
+		reader.close();
+
+		// print result
+		System.out.println(response.toString());
+		httpClient.close();
+	}
+
+	private static void sendPOST() throws IOException {
+
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpPost httpPost = new HttpPost(POST_URL);
+		httpPost.addHeader("User-Agent", USER_AGENT);
+
+		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+		
+		String tempStr = "{\"ROWS\":[{\"REQUEST_ID\":\"S|COMMON.AUTHLOGIN_S\",\"PARAMETER\":{\"USERID\":\"sb0950\",\"USERPW\":\"sbsb7356\",\"APPID\":\"API_WEB\",\"DEVICEID\":\"API_NOMU_M0079\",\"REMARK\":\"IE 8.0\"}}]}";
+		urlParameters.add(new BasicNameValuePair("JSON", tempStr));
+		
+		HttpEntity postParams = new UrlEncodedFormEntity(urlParameters);
+		httpPost.setEntity(postParams);
+
+		CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+		
+		
+		
+		System.out.println("POST Response Status:: "
+				+ httpResponse.getStatusLine().getStatusCode());
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				httpResponse.getEntity().getContent(),"UTF-8"));
+
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = reader.readLine()) != null) {
+			response.append(inputLine);
+		}
+		
+		reader.close();
+
+
+		System.out.println(response.toString());
+		httpClient.close();
+	
+		
+	}
 
 }
